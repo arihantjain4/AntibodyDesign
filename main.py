43662d735f70df10f1c7246e1339c184478c2d32
Arihant Jain <jainarihantmr@gmail.com>
2023-04-14 16:02:40 -0700

    utilize GRU

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import numpy as np
-from tensorflow import keras
-from keras.models import Sequential
-from keras.layers import GRU # TODO: change to GRU
 from attention_decoder import AttentionDecoder
+import numpy as np
+from keras.models import Sequential
+from keras.layers import GRU 
 from random import randint
 
 # generate a sequence of random integers
@@ -11,7 +10,7 @@ def generate_sequence(length, n_unique):
 
 
 # one hot encode sequence
-def one_hot_encode(sequence, n_unique):
+def encode_vector(sequence, n_unique):
     encoding = list()
     for value in sequence:
         vector = [0 for _ in range(n_unique)]
@@ -20,8 +19,7 @@ def one_hot_encode(sequence, n_unique):
     return np.array(encoding)
 
 
-# decode a one hot encoded string
-def one_hot_decode(encoded_seq):
+def decode_vector(encoded_seq):
     return [np.argmax(vector) for vector in encoded_seq]
 
 
@@ -31,8 +29,8 @@ def get_pair(n_in, n_out, cardinality):
     sequence_in = generate_sequence(n_in, cardinality)
     sequence_out = sequence_in[:n_out] + [0 for _ in range(n_in - n_out)]
     # one hot encode
-    X = one_hot_encode(sequence_in, cardinality)
-    y = one_hot_encode(sequence_out, cardinality)
+    X = encode_vector(sequence_in, cardinality)
+    y = encode_vector(sequence_out, cardinality)
     # reshape as 3D
     X = X.reshape((1, X.shape[0], X.shape[1]))
     y = y.reshape((1, y.shape[0], y.shape[1]))
@@ -41,34 +39,34 @@ def get_pair(n_in, n_out, cardinality):
 
 def run():
     # configure problem
-    n_features = 50
-    n_timesteps_in = 5
-    n_timesteps_out = 2
+    features = 50
+    in_step = 5
+    out_step = 2
 
     # define model
     model = Sequential()
-    model.add(GRU(150, input_shape=(n_timesteps_in, n_features), return_sequences=True))
-    model.add(AttentionDecoder(150, n_features))
+    model.add(GRU(150, input_shape=(in_step, features), return_sequences=True))
+    model.add(AttentionDecoder(150, features))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     # train GRU
     for epoch in range(5000):
         # generate new random sequence
-        X, y = get_pair(n_timesteps_in, n_timesteps_out, n_features)
+        X, y = get_pair(in_step, out_step, features)
         # fit model for one epoch on this sequence
         model.fit(X, y, epochs=1, verbose=2)
     # evaluate GRU
     total, correct = 100, 0
     for _ in range(total):
-        X, y = get_pair(n_timesteps_in, n_timesteps_out, n_features)
+        X, y = get_pair(in_step, out_step, features)
         yhat = model.predict(X, verbose=0)
-        if np.array_equal(one_hot_decode(y[0]), one_hot_decode(yhat[0])):
+        if np.array_equal(decode_vector(y[0]), decode_vector(yhat[0])):
             correct += 1
     print('Accuracy: %.2f%%' % (float(correct) / float(total) * 100.0))
     # spot check some examples
     for _ in range(10):
-        X, y = get_pair(n_timesteps_in, n_timesteps_out, n_features)
+        X, y = get_pair(in_step, out_step, features)
         yhat = model.predict(X, verbose=0)
-        print('Expected:', one_hot_decode(y[0]), 'Predicted', one_hot_decode(yhat[0]))
+        print('Expected:', decode_vector(y[0]), 'Predicted', decode_vector(yhat[0]))
 
 
 if __name__ == "__main__":
